a97ca9723393cf3c9dd445a6b3b01b49dd858b8b
Maverik2 <therealfolkblues@hotmail.es>
2013-03-12 22:54:50 +0100

    Update Terrain.sql

--- Gem/GEM/Text/ES_ES/Terrain.sql
+++ Gem/GEM/Text/ES_ES/Terrain.sql
@@ -1,1 +1,11 @@
---·
+Películas.
+Una película, filme o proyección cinematográfica, es como se llama a una secuencia de imágenes fotográficas tomadas con una cámara, que reproducidas mediante un proyector en una pantalla, crean la ilusión de imágenes en movimiento. Se crea captando escenas reales con una cámara de cine, reproduciendo secuencias de dibujos, de  modelos y maquetas en miniatura, por medio de CGIs y animación por ordenador, o por una combinación de todas o alguna de estas técnicas y efectos visuales. El proceso de hacer cine es un arte y también constituye una gran industria.
+Esto mejorará la [ICON_STRENGTH] defensa de cualquier unidad militar destinada en esta casilla, y quitara 20 de vida a los enemigos adyacentes.
+[COLOR_NEGATIVE_TEXT] Cuesta [ENDCOLOR] 3 [ICON_GOLD] de oro por turno de mantenimiento. [NEWLINE] [NEWLINE] 50% [ICON_STRENGTH] de fuerza defensiva para cualquier unidad estacionada en la casilla, y quita 20 de vida a los enemigos adyacentes.
+[COLOR_NEGATIVE_TEXT] Cuesta [ENDCOLOR] 3 [ICON_GOLD] de oro por turno de mantenimiento. [NEWLINE] [NEWLINE] Una fortaleza es una mejora especial que aumenta la bonificación defensiva de una casilla, y quita 20 de vida a los enemigos adyacentes. Sin embargo, estos efectos no se aplican en el territorio enemigo.
+Proporciona 100 [ICON_GOLD] de oro al primer jugador que lo descubra.
+Islas
+Islas
+Una isla es cualquier territorio sub-continental de tierra que esta rodeado por agua. Las islas muy pequeñas pueden ser llamadas islotes, cayos  o isleta.
+{TXT_KEY_BUILD_CITADEL_HELP}
+[ICON_STRENGTH] 100% [COLOR_POSITIVE_TEXT] de defensa [ENCOLOR] en esta casilla. [NEWLINE] [ICON_RANGED_STRENGTH] Quita 30 [COLOR_POSITIVE_TEXT] de vida [ENCOLOR] por turno a los enemigos adyacentes [NEWLINE] [ICON_CULTURE] Expande las fronteras a las casillas de alrededor. [NEWLINE] [ICON_LOCKED] Requiere esta en territorio amigo.
