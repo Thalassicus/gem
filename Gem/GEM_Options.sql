e6c1197d533964732db5cda7886abb2717ef78ff
Jakub Pierzchała <jakub.pierzchala@gmail.com>
2013-03-15 21:41:13 +0100

    Revert "Bringing up to date with the origin"
    This reverts commit 1abeeaff9303c3b0e73e3fffe723545846e50f31.

---
+++ Gem/GEM_Options.sql
@@ -0,0 +1,113 @@
+/*
+
+You can change most options in this file at any time, unless indicated otherwise.
+Changes take effect the next time you start or load a game with CiVUP/VEM.
+
+For example, if you are using the "Citystate Diplomacy" mod change the lines that read:
+
+	INSERT INTO Civup (Type, Value)
+	VALUES ('USING_CSD', 0);
+
+...change to...
+
+	INSERT INTO Civup (Type, Value)
+	VALUES ('USING_CSD', 1);
+
+Then start a new game.
+
+*/
+
+
+-------------
+-- Options --
+-------------
+
+
+/*
+CityState Diplomacy Mod Compatibility
+Change this ONLY before starting a game, NOT mid-game.
+0 = not using CSD and VEM
+1 = using CSD and VEM
+*/
+INSERT OR REPLACE INTO Civup (Type, Value)
+VALUES ('USING_CSD', 0);
+
+
+/*
+Good For
+
+These show the "good for" value of objects on their tooltips.
+This is helpful for people new to the game or analyzing balance.
+
+1 = show Good For
+0 = hide Good For
+*/
+UPDATE Civup SET Value = 1 WHERE Type = 'SHOW_GOOD_FOR_UNITS';
+UPDATE Civup SET Value = 1 WHERE Type = 'SHOW_GOOD_FOR_BUILDINGS';
+UPDATE Civup SET Value = 1 WHERE Type = 'SHOW_GOOD_FOR_POLICIES';
+UPDATE Civup SET Value = 1 WHERE Type = 'SHOW_GOOD_FOR_TECHS';
+UPDATE Civup SET Value = 1 WHERE Type = 'SHOW_GOOD_FOR_BUILDS';
+
+UPDATE Civup SET Value = 0 WHERE Type = 'SHOW_GOOD_FOR_RAW_NUMBERS';
+UPDATE Civup SET Value = 0 WHERE Type = 'SHOW_GOOD_FOR_AI_NUMBERS';
+
+
+/*
+Barbarians Upgrade
+1 = barbarians upgrade in camps
+0 = barbarians do not upgrade 
+*/
+INSERT INTO Civup (Type, Value)
+VALUES ('BARBARIANS_UPGRADE', 1);
+
+
+/*
+Barbarians Heal
+1 = barbarians heal when fortified
+0 = barbarians do not heal
+*/
+INSERT INTO Civup (Type, Value)
+VALUES ('BARBARIANS_HEAL', 1);
+
+
+/*
+Human-vs-barbarian combat bonus.
+*/
+UPDATE HandicapInfos SET BarbarianBonus = 150 WHERE Type = 'HANDICAP_SETTLER';
+UPDATE HandicapInfos SET BarbarianBonus =  50 WHERE Type = 'HANDICAP_CHIEFTAIN';
+UPDATE HandicapInfos SET BarbarianBonus =  20 WHERE Type = 'HANDICAP_WARLORD';
+UPDATE HandicapInfos SET BarbarianBonus =  15 WHERE Type = 'HANDICAP_PRINCE';
+UPDATE HandicapInfos SET BarbarianBonus =  15 WHERE Type = 'HANDICAP_KING';
+UPDATE HandicapInfos SET BarbarianBonus =  15 WHERE Type = 'HANDICAP_EMPEROR';
+UPDATE HandicapInfos SET BarbarianBonus =  15 WHERE Type = 'HANDICAP_IMMORTAL';
+UPDATE HandicapInfos SET BarbarianBonus =  15 WHERE Type = 'HANDICAP_DEITY';
+
+UPDATE Defines SET Value = 60 WHERE Name = 'BARBARIAN_MAX_XP_VALUE';
+
+
+/*
+Minimum distance (in tiles) between cities.
+*/
+UPDATE Defines
+SET Value = 2
+WHERE Name = 'MIN_CITY_RANGE';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--
+-- Do not change this
+UPDATE LoadedFile SET Value=1 WHERE Type='GEM_Options.sql';
