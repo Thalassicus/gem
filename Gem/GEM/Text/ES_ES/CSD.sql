586e433a73cedc403238f1b5e17b3906e69cb3d2
Jairo Alonso Velásquez <jairoalonso25@gmail.com>
2013-03-19 18:57:41 -0500

    Update some sql files
    Changed Icon positions and some typos

--- Gem/GEM/Text/ES_ES/CSD.sql
+++ Gem/GEM/Text/ES_ES/CSD.sql
@@ -1,7 +1,7 @@
-INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-13'), 'TXT_KEY_POLICY_PHILANTHROPY_HELP_EXTRA', '[NEWLINE]+1 [ICON_HAPPINESS_1] Felicidad de la Ciudad por la Escuela de Escribas.', '', '');
-INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-13'), 'TXT_KEY_POLICY_CULTURAL_DIPLOMACY_HELP_EXTRA', '[NEWLINE]+3 [ICON_CULTURE] Cultura por la Oficina de Asuntos Exteriores.', '', '');
-INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-13'), 'TXT_KEY_POLICY_AESTHETICS_HELP_EXTRA', '[NEWLINE]+20% [ICON_GOLD] Oro por la Prensa de Gutenberg.', '', '');
-INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-13'), 'TXT_KEY_POLICY_SCHOLASTICISM_HELP_EXTRA', '[NEWLINE]+10% [ICON_RESEARCH] Ciencia por el Servicio Postal.', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-13'), 'TXT_KEY_POLICY_PHILANTHROPY_HELP_EXTRA', '[NEWLINE]+1 de felicidad [ICON_HAPPINESS_1] de la Ciudad por la Escuela de Escribas.', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-13'), 'TXT_KEY_POLICY_CULTURAL_DIPLOMACY_HELP_EXTRA', '[NEWLINE]+3 cultura [ICON_CULTURE] por la Oficina de Asuntos Exteriores.', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-13'), 'TXT_KEY_POLICY_AESTHETICS_HELP_EXTRA', '[NEWLINE]+20% de oro [ICON_GOLD] por la Prensa de Gutenberg.', '', '');
+INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-13'), 'TXT_KEY_POLICY_SCHOLASTICISM_HELP_EXTRA', '[NEWLINE]+10% ciencia [ICON_RESEARCH] por el Servicio Postal.', '', '');
 INSERT INTO Civup_Language_ES_ES (DateModified, Tag, Text, Gender, Plurality) VALUES (date('2013-03-13'), 'TXT_KEY_POLICY_TRADE_PACT_HELP_EXTRA', '[NEWLINE]+2 Mensajeros.', '', '');
 
 UPDATE LoadedFile SET Value=1 WHERE Type='GEM_CSD.sql';
